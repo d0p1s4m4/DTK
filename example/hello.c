599132fd0a9039d4e237b512898f87065ec8ee9d
d0p1 <contact@d0p1.eu>
2022-10-11 12:05:01 +0200

    feat: create blank window

--- example/hello.c
+++ example/hello.c
@@ -1,20 +1,51 @@
-#include <dtk/application.h>
+/*
+ * Copyright (c) 2022, d0p1
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <dtk/window.h>
 #include <stdlib.h>
 
 int
 main(int argc, char **argv)
 {
-    DtkApplication *app;
+	DtkApplication *app;
 
-    app = dtk_application_new(argc, argv);
+	app = dtk_application_new(argc, argv);
 	if (app == NULL)
 	{
 		return (EXIT_FAILURE);
 	}
 
-    dtk_application_run(app);
+	dtk_window_new(app);
 
-    dtk_application_destroy(app);
+	dtk_application_run(app);
 
-    return (EXIT_SUCCESS);
+	dtk_application_destroy(app);
+
+	return (EXIT_SUCCESS);
 }
--- include/dtk/data/vector.h
+++
@@ -1,9 +0,0 @@
-#ifndef DTK_DATA_VECTOR_H
-# define DTK_DATA_VECTOR_H 1
-
-#define Vec(x) typedef struct
-{
-
-} Vec;
-
-#endif /* !DTK_DATA_VECTOR_H */
